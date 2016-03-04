dc7ead360a22d59ef11f4e5d36e7bb441670823a
Duncan McGreggor <oubiwann@gmail.com>
2016-03-04 09:57:19 -0600

    Updated license and deps.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject logic-tut "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :description "HackDay Project: Logic and Relations Programming with Clojure's core.logic."
+  :url "https://github.com/oubiwann/hackday-logic-tutorial"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.logic "0.8.10"]
+                 [org.clojure/tools.namespace "0.2.11"]])
