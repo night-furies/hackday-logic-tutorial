204650d234f277544431de31ceee61f2aee53b54
Duncan McGreggor <oubiwann@gmail.com>
2016-03-04 10:57:08 -0600

    Removed sensor from core.

--- src/logic_tut/core.clj
+++ src/logic_tut/core.clj
@@ -4,39 +4,3 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
-
-
-(def sensor-graph
-  [; Graph of sensor hierarchies for Landsat and MODIS
-   [:base-sensor ]
-   
-   [:modis ]
-   
-   [:landsat ]
-   [:landsat :pixel-size 30 ]
-   [:landsat :pixel-size-unit "meters" ]
-   
-   [:landsat-4 ]
-   [:landsat-5 ]
-   [:landsat-7 ]
-   [:landsat-8 ]
-   
-   [:modis-aqua ]
-   
-   [:modis-terra-09A1 ]
-   [:modis-terra-09GA ]
-   [:modis-terra-09GQ ]
-   [:modis-terra-09Q1 ]
-   [:modis-terra-13A1 ]
-   [:modis-terra-13A2 ]
-   [:modis-terra-13A3 ]
-   [:modis-terra-13Q1 ]
-   
-   [:modis-aqua-09A1 ]
-   [:modis-aqua-09GA ]
-   [:modis-aqua-09GQ ]
-   [:modis-aqua-09Q1 ]
-   [:modis-aqua-13A1 ]
-   [:modis-aqua-13A2 ]
-   [:modis-aqua-13A3 ]
-   [:modis-aqua-13Q1 ]]
